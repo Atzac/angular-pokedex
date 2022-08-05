d0193bf59a2edc0de2524950440ee8edbdaf5c54
Atzac <isaacquidutemelo13@gmail.com>
2022-08-05 16:16:53 -0300

    list pokemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get allPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap( res => res ),
+      tap( res => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status= res
+          );
+        })
+      })
+    )
+  }
+
+  public apiGetPokemons( pokeUrl: string ): Observable<any>  {
+    return this.http.get<any>(pokeUrl).pipe(
+      map(res => res)
+    )
+  }
 }
