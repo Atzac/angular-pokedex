84de90ec5bdfc044283ca6a2a895658bfdc2ce65
Atzac <isaacquidutemelo13@gmail.com>
2022-08-08 17:21:02 -0300

    repais finish

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -22,10 +22,10 @@ export class DetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon() {
+  public getPokemon() {
     const id = this.activedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`)
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`)
